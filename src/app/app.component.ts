0eb79656d4e2faf9bca45fc6510ac845cb4d4069
Masahiro Okubo <stmhamachiii@gmail.com>
2020-10-24 11:27:35 +0900

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularFile';
+    title = 'angularFile';
+
+    constructor(private readonly router: Router) {
+    }
+
+    public async downloadFile() {
+        const response = await fetch('assets/sample.jpg');
+        const blob =  await response.blob();
+        const url = window.URL.createObjectURL(blob);
+
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        a.setAttribute('style', 'display: none');
+        a.href = url;
+        a.download = 'Angular_sample_file';
+        a.click();
+        window.URL.revokeObjectURL(url);
+        this.router.navigate(['/homepage']);
+    }
 }
